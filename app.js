ee1047f7a3f4b3ce7f39cc24ff4feb3036191c9f
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-04 10:02:52 +0530

    new2

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let fullName = "Sandaru Sadishan";
-let age = 24;
+let age = 20;
 let isGraduate = true;
 
 console.log("Name: " + fullName);
 console.log("Age: " + age);
 console.log("Graduate: " + isGraduate);
+
+
+if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a child");
+}
