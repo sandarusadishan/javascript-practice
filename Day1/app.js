f2249cf95901bc8423b8653c43f536fd4d0a8117
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-06 15:09:19 +0530

    new9

--- Day1/app.js
+++ Day1/app.js
@@ -1,9 +1,9 @@
 let Age = 40;
-
+console.log(" Group : "); 
 if (Age <=18) {
-  console.log("You are Child!");
+  console.log("Child!");
 }if (Age > 18 && Age <=25) {
-  console.log("You are Teen !");
+  console.log("Teen!");
   } else {
-  console.log("You are Adult!");
+  console.log("Adult!");
 }
