8de35d085fe4de16141ba81f92a223d9018891ef
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-06 15:20:49 +0530

    new10

--- Day1/app.js
+++ Day1/app.js
@@ -1,9 +1,29 @@
 let Age = 40;
-console.log(" Group : "); 
-if (Age <=18) {
-  console.log("Child!");
-}if (Age > 18 && Age <=25) {
-  console.log("Teen!");
-  } else {
-  console.log("Adult!");
+console.log(" Group : ");
+if (Age <= 18) {
+  console.log("You are Child!");
+}
+if (Age > 18 && Age <= 25) {
+  console.log("You are Teen !");
+} else {
+  console.log("You are Adult!");
+}
+
+
+
+let Grade = 85;
+
+if (Grade >= 75) {
+  console.log("A");
+}
+if (Grade < 75 && Grade >= 65) {
+  console.log("B");
+}
+if (Grade < 65 && Grade >= 55) {
+  console.log("C");
+}
+if (Grade < 55 && Grade >= 45) {
+  console.log("S");
+} else {
+  console.log("F");
 }
