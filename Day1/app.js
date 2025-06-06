21094abcaf05ecee0a42187196d90924cd77e38f
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-06 14:45:38 +0530

    new6

--- Day1/app.js
+++ Day1/app.js
@@ -2,11 +2,9 @@ let fullName = "Sandaru Sadishan";
 let age = 20;
 let isGraduate = true;
 
-console.log("Name: " + fullName);
-console.log("Age: " + age);
-console.log("Graduate: " + isGraduate);
 
 
+console.log("My name is " + fullName + ", I am " + age + " years old. Graduate : " + isGraduate + ".");
 
 
 
