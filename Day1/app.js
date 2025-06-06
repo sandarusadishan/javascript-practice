cfd8534663a06d6696243098707ed1d7b7eef88d
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-06 14:41:15 +0530

    new5

--- Day1/app.js
+++ Day1/app.js
@@ -7,8 +7,6 @@ console.log("Age: " + age);
 console.log("Graduate: " + isGraduate);
 
 
-if (age >= 18) {
-  console.log("You are an adult");
-} else {
-  console.log("You are a child");
-}
+
+
+
---
+++ new/new.py
@@ -0,0 +1,1 @@
+print("Hello, Shan! Python is working.")
