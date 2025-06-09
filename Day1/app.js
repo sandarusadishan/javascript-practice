66724f0323b9d0ed8b20e184f3ea02f7996749c6
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-09 12:35:03 +0530

    new14

--- Day1/app.js
+++ Day1/app.js
@@ -6,12 +6,11 @@ const rl = readline.createInterface({
 });
 
 rl.question("Enter your Age: ", function (ageInput) {
-  const Age = parseInt(ageInput); // convert string to number
+  const Age = parseInt(ageInput); 
 
   rl.question("Enter your Grade: ", function (gradeInput) {
-    const Grade = parseInt(gradeInput); // convert string to number
+    const Grade = parseInt(gradeInput); 
 
-    // Age Group
     if (Age <= 18) {
       console.log("Age Group: You are Child!");
     } else if (Age <= 25) {
@@ -20,7 +19,7 @@ rl.question("Enter your Age: ", function (ageInput) {
       console.log("Age Group: You are Adult!");
     }
 
-    // Grade Check
+   
     if (Grade >= 75) {
       console.log("Grade: A");
     } else if (Grade >= 65) {
@@ -33,13 +32,12 @@ rl.question("Enter your Age: ", function (ageInput) {
       console.log("Grade: Fail");
     }
 
-    // Job Eligibility
     if (Age >= 22 && Grade >= 45) {
       console.log("Status: Ready for job!");
     } else {
       console.log("Status: Not ready for job!");
     }
 
-    rl.close(); // close input
+    rl.close(); 
   });
 });
