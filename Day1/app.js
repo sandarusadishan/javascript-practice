605eea440c21774708a42c6f0c543ba4b42197c4
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-09 10:23:54 +0530

    new12

--- Day1/app.js
+++ Day1/app.js
@@ -1,35 +1,36 @@
-let Age = 20;
-console.log(" Group : ");
+let Age = 25;
+
 if (Age <= 18) {
-  console.log("You are Child!");
+  console.log("Age Griup :You are Child!");
 }
 if (Age > 18 && Age <= 25) {
-  console.log("You are Teen !");
+  console.log("Age Group : You are Teen !");
 } else {
-  console.log("You are Adult!");
+  console.log("Age Group : You are Adult!");
 }
 
-let Grade = 85;
+let Grade = 100;
 
 if (Grade >= 75) {
-  console.log("A");
+  console.log("Grade : A");
 }
 if (Grade < 75 && Grade >= 65) {
-  console.log("B");
+  console.log("Grade : B");
 }
 if (Grade < 65 && Grade >= 55) {
-  console.log("C");
+  console.log("Grade : C");
 }
 if (Grade < 55 && Grade >= 45) {
-  console.log("S");
+  console.log("Grade :S");
 } else {
+  console.log("Grade : Fail");
 }
 
-console.log("Status :");
 
-if (Age >= 22) {
-  console.log("Ready for job!");
+
+if (Age >= 22 && Grade >= 45) {
+  console.log("Status : Ready for job!");
 } else {
-  console.log("Not ready for job!");
+  console.log("Status : Not ready for job!");
 }
 
