b4b2fa43bc6b90d411bc41a05ad0a4eca1bdfbbf
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-11 11:45:47 +0530

    new17

---
+++ day01/app.js
@@ -0,0 +1,21 @@
+// Variables
+let name = "John";
+const age = 25;
+var isStudent = true;
+
+// Log to console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+// Data types
+let height = 5.9; // number
+let hobbies = ["reading", "gaming","Dancing","Playing Cricket"]; // array
+let person = { name: "John", age: 25 }; // object
+let grade = null;
+let city;
+
+console.log("Height  : " + height); // number
+console.log("Hobbies : " + hobbies); // object
+console.log("Grade   : " + grade); // object (special case)
+console.log("City    : " + city); // undefined
