696b5361450ece600bbf773e79a3557023c20687
sandarusadishan <sandarusadishan0404@gmail.com>
2025-06-11 15:21:07 +0530

    new22

--- day01/index.java
+++ day01/index.java
@@ -2,14 +2,9 @@ import java.util.Scanner;
 
 public class index {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter your name : " + sc.nextInt());
-        int a = 5;
-        if (a > 0) {
-            System.out.println("Positive");
-        }
-        else{
-            System.out.println("Negative");
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+
     }
 }
---
+++ day01/new1.java
@@ -0,0 +1,29 @@
+public class new1 {
+    public static void main(String[] args) {
+
+        // String name = "Sandaru";
+        // int age = 20;
+        // double height = 1.75;
+        // boolean isStudent = true;
+
+        // System.out.println("Name : " + name);
+        // System.out.println("Age : " + age);
+        // System.out.println("Height : " + height);
+        // System.out.println("Is Student : " + isStudent);
+
+        int a = 16;
+        int b = 4;
+
+        int sum = a + b;
+        int fifference = a -b;
+        int product = a*b;
+        int quotient = a/b;
+        int remainder = a%b;
+
+        System.out.println("Sum : "+ sum );
+        System.out.println("Difference : "+ difference );
+        System.out.println("Product : "+ product );
+        System.out.println("Quotient : "+ quotient );
+        System.out.println("Remainder : "+ remainder );
+    }
+}
